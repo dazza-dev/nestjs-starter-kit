a9f50965118f07c9e5b84434e3f027fe189589c8
Dazza <adaza90@outlook.com>
2025-12-08 15:35:09 -0500

    Improvements

--- src/modules/acl/roles/repositories/roles.repository.ts
+++ src/modules/acl/roles/repositories/roles.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma } from '@/common/prisma/generated/client';
 import { prisma } from '@/common/prisma/prisma.client';
 import {
   prismaPaginate,
