eb1e7701a07430e6e1a43e7d10affbb71370a9e5
Dazza <adaza90@outlook.com>
2025-12-08 11:09:35 -0500

    Improvements

--- src/common/prisma/prisma.service.ts
+++ src/common/prisma/prisma.service.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '../../../prisma/generated/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from './generated/client';
 
-@Injectable()
-export class PrismaService extends PrismaClient {}
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
